f1d7b41e0f81b024b69b9f83b40ada2f34b6cc60
Melle Sieswerda <melle@sieswerda.net>
2019-01-15 13:54:02 +0100

    UI now built with create-react-app (CRA)

---
+++ pytaskmanager/static/precache-manifest.7e638e750cb2cef5167381a109691e6c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e90cb5cb7d6e31781763adf0b007f51",
+    "url": "/static/static/media/united_sans_medium.7e90cb5c.woff2"
+  },
+  {
+    "revision": "160527a1dbdcd5c37dab36c086b21ff5",
+    "url": "/static/static/media/united_sans_light.160527a1.woff2"
+  },
+  {
+    "revision": "bc2445d5bc77a73d75295c096dc2c0ff",
+    "url": "/static/static/media/fira_mono.bc2445d5.woff2"
+  },
+  {
+    "revision": "d7fea702c843b762d2c44593e157fd45",
+    "url": "/static/static/media/background.d7fea702.jpg"
+  },
+  {
+    "revision": "22d3d44b7c02275b5a5f",
+    "url": "/static/static/js/runtime~main.22d3d44b.js"
+  },
+  {
+    "revision": "fe9ba43936f389e82a95",
+    "url": "/static/static/js/main.fe9ba439.chunk.js"
+  },
+  {
+    "revision": "82eb44cc6c37cd11f821",
+    "url": "/static/static/js/1.82eb44cc.chunk.js"
+  },
+  {
+    "revision": "fe9ba43936f389e82a95",
+    "url": "/static/static/css/main.9c8fe17c.chunk.css"
+  },
+  {
+    "revision": "82eb44cc6c37cd11f821",
+    "url": "/static/static/css/1.89567ee7.chunk.css"
+  },
+  {
+    "revision": "bcc1da4e9344e82f252413228754e041",
+    "url": "/static/index.html"
+  }
+];
